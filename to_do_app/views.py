dbf49bfcd16eab2ea855e6d350438e67d7eb2a1a
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2023-08-12 01:46:39 +0600

    edit, delete done

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -13,9 +13,27 @@ class HomeView(ListView):
     context_object_name = "tasks"
     ordering = ["taskTitle"]
 
+    def get_queryset(self):
+        return TaskModel.objects.filter(is_completed=False)
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx.update({"sec_title": "Home"})
+        ctx.update({"sec_title": "Incomplete Tasks"})
+        return ctx
+
+
+class CompletedTaskView(ListView):
+    model = TaskModel
+    template_name = "home.html"
+    context_object_name = "tasks"
+    ordering = ["taskTitle"]
+
+    def get_queryset(self):
+        return TaskModel.objects.filter(is_completed=True)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update({"sec_title": "Complete Tasks"})
         return ctx
 
 
@@ -40,4 +58,15 @@ class TaskUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx.update({"sec_title": "Edit Task"})
+        return ctx
+
+
+class TaskDeleteView(DeleteView):
+    model = TaskModel
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx.update({"sec_title": "Delete Task"})
         return ctx
